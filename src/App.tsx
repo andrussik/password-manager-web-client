d48b787e4b1ef6d211cd3be04ef60b72d1f080f8
andre.stender <andre.stender@netgroup.com>
2022-02-28 01:21:55 +0200

    Initial commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,42 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { QueryClient, QueryClientProvider } from 'react-query';
+import { ToastContainer } from 'react-toastify';
+import AppRoutes from './routes/AppRoutes';
+import UserProvider from './UserContext';
+import 'react-toastify/dist/ReactToastify.css';
+import Header from './components/header/Header';
+import { BrowserRouter } from 'react-router-dom';
+
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      retry: false,
+      refetchOnWindowFocus: false,
+    },
+  },
+});
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+      <BrowserRouter>
+        <ToastContainer
+          position='top-center'
+          autoClose={5000}
+          hideProgressBar={false}
+          newestOnTop
+          closeOnClick
+          rtl={false}
+          pauseOnFocusLoss
+          draggable
+          pauseOnHover
+        />
+        <QueryClientProvider client={queryClient}>
+          <UserProvider>
+            <Header />
+            <AppRoutes />
+          </UserProvider>
+        </QueryClientProvider>
+      </BrowserRouter>
     </div>
   );
 }
